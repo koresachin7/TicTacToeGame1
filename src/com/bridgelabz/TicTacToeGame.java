7aaf881156dc574fda2f45e368386bc2d26a249c
koresachin7 <koresachin7@gmail.com>
2021-09-11 09:33:27 +0530

    Added : Adding method to check iteration not go belog to 9

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,7 +1,22 @@
 package com.bridgelabz;
 
 public class TicTacToeGame {
+    static char[] board = new char[10]; // board size 10
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tac Toe Game");
+        createBoard();//calling method
+    }
+
+    /*Method use:
+     *in method data is character
+     *and using for loop to iteration from 1 to 9
+
+     */
+    public static char[] createBoard() {
+        for (int index = 1; index < board.length; index++) {
+            board[index] = ' ';
+        }
+        return board;
+
     }
 }
